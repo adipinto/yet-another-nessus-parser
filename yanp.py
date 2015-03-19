472f78b4d8fa502d6388ecd93ab6127508d1bbf2
Alessandro <adipinto@users.noreply.github.com>
2015-03-19 21:23:06 +0100

    Update yanp.py

--- yanp.py
+++ yanp.py
@@ -6,7 +6,7 @@ Copyright notice
 ================
 
 Copyright (C) 2012
-     Alessandro Di Pinto             <alessandro.dipinto@security.dico.unimi.it>
+     Alessandro Di Pinto             <alessandro.dipinto@artificialstudios.org>
 
  This program is free software: you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
